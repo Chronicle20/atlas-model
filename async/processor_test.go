20b347997ec3faeb1101f8503012fc575c5d44a3
Chronicle20 <a.chronicle.20@gmail.com>
2024-08-16 11:26:56 -0400

    Add AsyncSlice test.

---
+++ async/processor_test.go
@@ -0,0 +1,46 @@
+package async
+
+import (
+	"context"
+	"github.com/Chronicle20/atlas-model/model"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestAsyncSlice(t *testing.T) {
+	items := []uint32{1, 2, 3, 4, 5}
+
+	wg := sync.WaitGroup{}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		results, err := AwaitSlice(model.SliceMap(model.FixedProvider(items), AsyncTestTransformer))()
+		if err != nil {
+			t.Fatal(err)
+		}
+		for _, result := range results {
+			found := false
+			for _, item := range items {
+				if item == result {
+					found = true
+					break
+				}
+			}
+			if !found {
+				t.Fatalf("Invalid item.")
+			}
+		}
+		t.Logf("Here 2")
+	}()
+	wg.Wait()
+	t.Logf("Here 1")
+}
+
+func AsyncTestTransformer(m uint32) (Provider[uint32], error) {
+	return func(ctx context.Context, rchan chan uint32, echan chan error) {
+		time.Sleep(time.Duration(50) * time.Millisecond)
+		rchan <- m
+	}, nil
+}
